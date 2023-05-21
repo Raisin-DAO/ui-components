4488882b0e66c9455259040bc992aa4d9c3f8553
Saúl García Céspedes <saul@dixwe.com>
2023-05-21 15:58:31 +0200

    chore: remove my component

--- src/src/components/MyComponent/MyComponent.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { MyComponent } from './MyComponent';
-
-test('renders text prop', () => {
-  render(<MyComponent text="Hello, World!" />);
-  const textElement = screen.getByText(/Hello, World!/i);
-  expect(textElement).toBeInTheDocument();
-});
